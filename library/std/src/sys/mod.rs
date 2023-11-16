c6d2e4f83cb0a0ec157206a5c34c53a18a9f9737
Erik Kaneda <erik@risczero.com>
2023-11-15 16:57:45 -0800

    zkvm: add partial std support
    Co-authored-by: Frank Laub <flaub@risc0.com>
    Co-authored-by: nils <nils@risc0.com>
    Co-authored-by: Victor Graf <victor@risczero.com>
    Co-authored-by: weikengchen <w.k@berkeley.edu>

--- library/std/src/sys/mod.rs
+++ library/std/src/sys/mod.rs
@@ -53,6 +53,9 @@ cfg_if::cfg_if! {
     } else if #[cfg(all(target_vendor = "fortanix", target_env = "sgx"))] {
         mod sgx;
         pub use self::sgx::*;
+    } else if #[cfg(target_os = "zkvm")] {
+        mod zkvm;
+        pub use self::zkvm::*;
     } else {
         mod unsupported;
         pub use self::unsupported::*;
