7e5f986be607342c9130228dfc7796f81c4adeb4
leruaa <aurelien.catinon@gmail.com>
2025-07-01 05:36:52 -0700

    feat: Succinct Rust toolchain 1.93.0

--- library/std/src/sys/pal/zkvm/abi.rs
+++ library/std/src/sys/pal/zkvm/abi.rs
@@ -1,10 +1,10 @@
-//! ABI definitions for symbols exported by risc0-zkvm-platform.
+//! ABI definitions for symbols exported by sp1-zkvm.
 
-// Included here so we don't have to depend on risc0-zkvm-platform.
+// Included here so we don't have to depend on sp1-zkvm.
 //
 // FIXME: Should we move this to the "libc" crate?  It seems like other
 // architectures put a lot of this kind of stuff there.  But there's
-// currently no risc0 fork of the libc crate, so we'd either have to
+// currently no succinct fork of the libc crate, so we'd either have to
 // fork it or upstream it.
 
 #![allow(dead_code)]
@@ -19,26 +19,9 @@ pub mod fileno {
 }
 
 unsafe extern "C" {
-    // Wrappers around syscalls provided by risc0-zkvm-platform:
-    pub fn sys_halt();
-    pub fn sys_output(output_id: u32, output_value: u32);
-    pub fn sys_sha_compress(
-        out_state: *mut [u32; DIGEST_WORDS],
-        in_state: *const [u32; DIGEST_WORDS],
-        block1_ptr: *const [u32; DIGEST_WORDS],
-        block2_ptr: *const [u32; DIGEST_WORDS],
-    );
-    pub fn sys_sha_buffer(
-        out_state: *mut [u32; DIGEST_WORDS],
-        in_state: *const [u32; DIGEST_WORDS],
-        buf: *const u8,
-        count: u32,
-    );
-    pub fn sys_rand(recv_buf: *mut u32, words: usize);
+    // Wrappers around syscalls provided by sp1-zkvm-platform:
+    pub fn sys_rand(recv_buf: *mut u8, words: usize);
     pub fn sys_panic(msg_ptr: *const u8, len: usize) -> !;
-    pub fn sys_log(msg_ptr: *const u8, len: usize);
-    pub fn sys_cycle_count() -> usize;
-    pub fn sys_read(fd: u32, recv_buf: *mut u8, nrequested: usize) -> usize;
     pub fn sys_write(fd: u32, write_buf: *const u8, nbytes: usize);
     pub fn sys_getenv(
         recv_buf: *mut u32,
@@ -46,8 +29,6 @@ unsafe extern "C" {
         varname: *const u8,
         varname_len: usize,
     ) -> usize;
-    pub fn sys_argc() -> usize;
-    pub fn sys_argv(out_words: *mut u32, out_nwords: usize, arg_index: usize) -> usize;
 
     // Allocate memory from global HEAP.
     pub fn sys_alloc_words(nwords: usize) -> *mut u32;
