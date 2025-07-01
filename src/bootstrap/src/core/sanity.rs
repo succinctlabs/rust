85fb57ab5d785e9b18e6231a0d376c824fc2c089
leruaa <aurelien.catinon@gmail.com>
2025-07-01 05:36:52 -0700

    feat: Succinct Rust toolchain 1.96.0

--- src/bootstrap/src/core/sanity.rs
+++ src/bootstrap/src/core/sanity.rs
@@ -39,6 +39,7 @@ const STAGE0_MISSING_TARGETS: &[&str] = &[
     // just a dummy comment so the list doesn't get onelined
     "x86_64-unknown-linux-gnumsan",
     "x86_64-unknown-linux-gnutsan",
+    "riscv32im-succinct-zkvm-elf",
 ];
 
 /// Minimum version threshold for libstdc++ required when using prebuilt LLVM
