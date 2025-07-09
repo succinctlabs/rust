0b89d9af77b23e505f991d7c45428ee9dcb6a80f
nhtyy <n@push0.dev>
2025-07-09 11:51:30 -0700

    feat: 64-bit RISC-V support

---
+++ compiler/rustc_target/src/spec/targets/riscv64im_succinct_zkvm_elf.rs
@@ -0,0 +1,39 @@
+use crate::spec::{
+    Cc, LinkerFlavor, Lld, PanicStrategy, RelocModel, Target, TargetMetadata, TargetOptions,
+};
+
+pub(crate) fn target() -> Target {
+    Target {
+        data_layout: "e-m:e-p:64:64-i64:64-i128:128-n32:64-S128".into(),
+        llvm_target: "riscv64".into(),
+        metadata: TargetMetadata {
+            description: Some("Succinct's zero-knowledge Virtual Machine (RV64IM ISA)".into()),
+            tier: Some(3),
+            host_tools: Some(false),
+            std: None, // ?
+        },
+        pointer_width: 64,
+        arch: "riscv64".into(),
+
+        options: TargetOptions {
+            os: "zkvm".into(),
+            vendor: "succinct".into(),
+            linker_flavor: LinkerFlavor::Gnu(Cc::No, Lld::Yes),
+            linker: Some("rust-lld".into()),
+            cpu: "generic-rv64".into(),
+
+            max_atomic_width: Some(64),
+            atomic_cas: true,
+
+            features: "+m".into(),
+            llvm_abiname: "lp64".into(),
+            executables: true,
+            panic_strategy: PanicStrategy::Abort,
+            relocation_model: RelocModel::Static,
+            emit_debug_gdb_scripts: false,
+            eh_frame_header: false,
+            singlethread: true,
+            ..Default::default()
+        },
+    }
+}
--- src/bootstrap/src/core/sanity.rs
+++ src/bootstrap/src/core/sanity.rs
@@ -37,6 +37,7 @@ const STAGE0_MISSING_TARGETS: &[&str] = &[
     "riscv64a23-unknown-linux-gnu",
     // just a dummy comment so the list doesn't get onelined
     "riscv32im-succinct-zkvm-elf",
+    "riscv64im-succinct-zkvm-elf",
     "aarch64_be-unknown-hermit",
     "aarch64_be-unknown-none-softfloat",
 ];
